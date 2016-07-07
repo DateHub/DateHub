f3218c9192f7707b096c0fd31effa25d9a513554
junggue <junggue.yang@gmail.com>
2016-07-07 10:45:42 -0700

    [setup]add skeleton components for front-end

---
+++ src/components/notification_upcomingDate.js
@@ -0,0 +1,10 @@
+// UpcomingDate: this component will be displayed only if the upcomingdate button of notification component is clicked
+
+// Display: the closest date information
+
+export default const Notification_upcomingDate = (props) => {
+
+  return (
+
+  );
+};
