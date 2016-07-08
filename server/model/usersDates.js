79836876a9b6eff4de276578f4d3b57c66b141b3
LycheeXD <lycheeexd@gmail.com>
2016-07-07 22:37:06 -0700

    [database] add reviews model

--- server/model/usersDates.js
+++ server/model/usersDates.js
@@ -1,11 +1,13 @@
 var db = require('../db.js');
+var Users = require('./users.js');
+var Dates = require('./dates.js');
 
 module.exports = (function(){
 
   UsersDates = db.define('usersDates');
 
-  Users.belongsToMany(Dates, {through: UsersDates, foreignkey: 'id'})
-  Dates.belongsToMany(Users, {through: UsersDates, foreignkey: 'id'})
+  Users.belongsToMany(Dates, {through: UsersDates, foreignKey: 'id'});
+  Dates.belongsToMany(Users, {through: UsersDates, foreignKey: 'id'});
 
   UsersDates.sync();
   return UsersDates;
