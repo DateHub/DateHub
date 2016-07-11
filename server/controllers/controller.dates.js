976ffc6a1bf10ddb85a7b6bc64808a0e96f513dc
LycheeXD <lycheeexd@gmail.com>
2016-07-11 12:14:57 -0700

    [refactor] add lodash to dependencies, dates & sessions controller setup

--- server/controllers/controller.dates.js
+++ server/controllers/controller.dates.js
@@ -1,1 +1,17 @@
 var template = require('./controller.template.js');
+
+module.exports = (function() {
+  var dateControllers = Object.create(template);
+
+  dateControllers.path = '/api/dates';
+
+  dateControllers.methods.get = function(request, response) {
+
+  };
+
+  dateControllers.methods.post = function(request, response) {
+
+  };
+
+  return dateControllers;
+})();
--- server/controllers/controller.sessions.js
+++ server/controllers/controller.sessions.js
@@ -1,2 +1,17 @@
 var template = require('./controller.template.js');
 
+module.exports = (function() {
+  var sessionControllers = Object.create(template);
+
+  sessionControllers.path = '/api/sessions';
+
+  sessionControllers.methods.get = function(request, response) {
+
+  };
+
+  sessionControllers.methods.post = function(request, response) {
+
+  };
+
+  return sessionControllers;
+})();
