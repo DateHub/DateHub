fe660679bb98a03af0d565756e762e8662fb798e
junggue <junggue.yang@gmail.com>
2016-07-11 12:15:17 -0700

    [Refactor] revised variable names for database helper functions

--- server/controllers/controller.users.js
+++ server/controllers/controller.users.js
@@ -2,6 +2,7 @@ var template = require('./controller.template.js');
 
 module.exports = (function(){
   var userController = Object.create(template);
+  
   userController.path = '/api/users';
 
   userController.methods.get = function(request, response) {
@@ -11,5 +12,15 @@ module.exports = (function(){
   userController.methods.post = function(request, response) {
     
   };
+
+  userController.methods.put = function(request, response) {
+    
+  };
+
+  userController.methods.delete = function(request, response) {
+    
+  };
+
+  return userController;
 
 })();
---
+++ server/controllers/controller.usersDates.js
@@ -0,0 +1,24 @@
+var template = require('./controllers/controller.template.js');
+
+module.exports = (function(){
+  var usersDatesController = Object.create(template);
+
+  usersDatesController.path = '/api/usersDates';
+  
+  usersDatesController.methods.get = function(request, response) {
+    
+  };
+
+  usersDatesController.methods.post = function(request, response) {
+    
+  };
+
+  usersDatesController.methods.put = function(request, response) {
+    
+  };
+
+  usersDatesController.methods.delete = function(request, response) {
+    
+  };
+
+})();
