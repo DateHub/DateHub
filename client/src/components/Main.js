84a0c5766663d7d727f28613ce1eaa9c763e99ac
seanml <i.r.sean@gmail.com>
2016-07-12 20:28:38 -0700

    [bugfix] capitalized first letter of component names (fixed git ignorecase)

---
+++ client/src/components/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+const Main = React.createClass({
+  render() {
+    return (
+      <div>
+        {React.cloneElement(this.props.children, this.props)}
+      </div>
+    )
+  }
+});
+
+export default Main;
