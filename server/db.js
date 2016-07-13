bc19ecfd5762109760f9819049d013a65bb84b46
junggue <junggue.yang@gmail.com>
2016-07-12 19:56:42 -0700

    updated

--- server/db.js
+++ server/db.js
@@ -1,24 +1,9 @@
-// var Sequelize = require('sequelize');
+var Sequelize = require('sequelize');
 
-// module.exports = (function() {
-//   var db = new Sequelize('LycheeXD', null, null, {
-//     host: 'localhost',
-//     dialect: "postgres"
-//   });
-//   return db; 
-// })();
-//------------------------------------ just for the test from this line------------------------------------
-var db = require('sequelize');
-
-module.exports = (function(){
-var Sequelize = new db('postgres://admin:GGMJXMLEQKCGRJDG@aws-us-east-1-portal.12.dblayer.com:10859/compose');
-Sequelize
-  .authenticate()
-  .then(function(err) {
-    console.log('Connection has been established successfully.');
-  })
-  .catch(function (err) {
-    console.log('Unable to connect to the database:', err);
+module.exports = (function() {
+  var db = new Sequelize('LycheeXD', null, null, {
+    host: 'localhost',
+    dialect: "postgres"
   });
-  return Sequelize
+  return db; 
 })();
