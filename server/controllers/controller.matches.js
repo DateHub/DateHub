21d25b714b477abc9a3c0213f973286d64ea1663
Denny Jun <denny.6a756e@gmail.com>
2016-07-15 23:23:20 -0700

    [feature] get newest matches

--- server/controllers/controller.matches.js
+++ server/controllers/controller.matches.js
@@ -1,7 +1,10 @@
 var template = require('./controller.template.js');
 var DbHelper = require('../utils/dbHelper.js');
-var TinderHelper = require('../utils/tinderHelper.js');
-var Matches = require('../models/matches.js');
+var tinderHelper = require('../utils/tinderHelper.js');
+var Match = require('../models/matches.js');
+var User = require('../models/users.js');
+var _ = require('lodash');
+var db = require('../db.js');
 
 module.exports = (function() {
   var matchController = template.clone({
@@ -9,12 +12,73 @@ module.exports = (function() {
   });
   var router = matchController.router;
 
-  router.get('/', function(request, response) {
-
-  });
-
-  router.post('/', function(request, response) {
-
+  router.get('/new', function(request, response) {
+    var userId = request.session.user.id;
+    var newPeople = [];
+    User.findOne({
+      where: {
+        id: userId
+      },
+      attributes: ['lastUpdated']
+    }).then(function(result) {
+      return result.dataValues.lastUpdated;
+    }).then(function(lastUpdated) {
+      return User.findOne({
+        where: {
+          id: userId
+        }
+      }).then(function(user) {
+        return user.updateAttributes({lastUpdated: Date().now()});
+      }).then(function(user) {
+        return lastUpdated;
+      }).catch(function(error) {
+        console.log(error);
+      });
+    }).then(function(lastUpdated) {
+      var newMatches = tinderHelper.getMatches(request.session.token, lastUpdated);
+      var newPeoplePromies = _.map(newMatches, function(match) {
+        newPeople.push(match.person);
+        return new Promise(function(resolve, reject) {
+          return User.findOrCreate({
+            where: {
+              id: match.person.id
+            },
+            defaults: match.person})
+            .then(resolve)
+            .catch(reject);
+        });
+      });
+      return Promise.all(newPeople).then(function(values) {
+        return newMatches;
+      });
+    }).then(function(newMatches) {
+      return _.map(newMatches, function(match) {
+        return {
+          matchId: match.matchId,
+          userId1: userId,
+          userId2: match.person.id
+        };
+      });
+    }).then(function(newMatches) {
+      var matches = _.map(newMatches, function(match) {
+        return new Promise(function(resolve, reject) {
+          return Match.findOrCreate({
+            where: {
+              matchId: match.matchId
+            },
+            defaults: match})
+            .then(resolve)
+            .catch(reject);
+        });
+      });
+      return Promise.all(matches).then(function(values) {
+        return values;
+      });
+    }).then(function(results) {
+      response.send(newPeople);
+    }).catch(function(error) {
+      response.status(500).send(error);
+    });
   });
 
   return matchController;
