11ce200bd9c3dafda6349fa1fe86aed69b3fe854
junggue <junggue.yang@gmail.com>
2016-07-15 17:17:18 -0700

    updated

--- server/utils/dbHelper.js
+++ server/utils/dbHelper.js
@@ -7,56 +7,55 @@ module.exports = (function(){
   //POST
   //inserting data into a table
   var insertData = function(request, response, table, newData){
-    console.log(newData)
-    db.table.create(newData)
+    table.create(newData)
       .then(function(){
         response.status(200).send(data);
       })
-      .catch(function(){
+      .catch(function(err){
         response.status(500).send(err.message);
       });
   };
 
   //GET
-  //get all
-  var getAll = function(request, response, table) {
-    db.table.findAll()
+  var getAll = function(request, response, table, fields, value) {
+    //get all records
+    if(!fields && !value) {
+      table.findAll()
+        .then(function(data){
+          response.status(200).send(data);
+        })
+        .catch(function(err){
+          response.status(500).send(err.message);
+        });
+    } 
+    //get all records for specific data
+    else {
+      table.findAll({where:{field:value},
+        attributes: fields})
       .then(function(data){
         response.status(200).send(data);
       })
       .catch(function(err){
         response.status(500).send(err.message);
       });
+    }
   }
-
-  //get all data from a column
-  var getAllOfField = function(request, response, table, field) {
-    db.table.findAll({where:{field}})
-      .then(function(data){
-        response.status(200).send(data);
-      })
-      .catch(function(err){
-        response.status(500).send(err.message);
-      });
-  };
-
-
 
   // get a data of a row (one record)
   var getRecordById = function(request, response, table, id){
-    db.table.findById(id)
-    .then(function(data){
-      response.status(200).send(data);
-    })
-    .catch(function(err){
-      response.status(500).send(err.message);
-    });
+    table.findById(id)
+      .then(function(data){
+        response.status(200).send(data);
+      })
+      .catch(function(err){
+        response.status(500).send(err.message);
+      });
   };
 
-  // reference: http://stackoverflow.com/questions/36480587/sequelize-how-to-check-if-entry-exists-in-database
+  //check if the id exist in a table
   var isIdExist = function(table, id){
-    db.table.count({where: {id: id}})
-      .then(count => {
+    return table.count({where: {id: id}})
+      .then(function(count) {
         if (count != 0) {
           return true;
         }
@@ -65,8 +64,9 @@ module.exports = (function(){
   };
 
   //PUT
+  //update a record
   var updateData = function(request, response, table, updatedData ,id){
-    db.table.update(updatedData, {where:{id:id}})
+    table.update(updatedData, {where:{id:id}})
       .then(function(result){
         response.status(200).send("successfully updated");
       })
@@ -77,21 +77,38 @@ module.exports = (function(){
 
   //DELETE
   var deleteData = function(request, response, table, id){
-    db.table.findById(id)
-    .then(function(id){
-      id.destroy();
-      response.status(200).send("the id " + id + " is successfully delete.")
-    })
-    .catch(function(err){
-        response.status(500).send(err.message);
-    });
+    table.findById(id)
+      .then(function(id){
+        id.destroy();
+        response.status(200).send("the id " + id + " is successfully delete.")
+      })
+      .catch(function(err){
+          response.status(500).send(err.message);
+      });
+  };
+
+  var calculateAge = function(birthDate, otherDate) {
+    birthDate = new Date(birthDate);
+    otherDate = new Date(otherDate);
+
+    var years = (otherDate.getFullYear() - birthDate.getFullYear());
+
+    if (otherDate.getMonth() < birthDate.getMonth() || 
+        otherDate.getMonth() == birthDate.getMonth() && otherDate.getDate() < birthDate.getDate()) {
+        years--;
+    }
+    return years;
   };
   
 
   return {
     insertData: insertData,
-    getAllOfField: getAllOfField,
-    getRecordById: getRecordById
+    isIdExist:isIdExist,
+    updateData: updateData,
+    getAll: getAll,
+    getRecordById: getRecordById,
+    updateData: updateData,
+    calculateAge: calculateAge
   };
 
 })();
